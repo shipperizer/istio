e56faf4fe585dded82a3dc6385422977ab9ec269
Romain Lenglet <romain@covalent.io>
2019-08-14 14:19:53 -0700

    Implement a Pilot plugin to inject the Cilium filters
    Configure the Cilium listener filter on the actual listen socket
    
    Signed-off-by: Romain Lenglet <romain@covalent.io>

--- pilot/pkg/networking/plugin/plugin.go
+++ pilot/pkg/networking/plugin/plugin.go
@@ -43,6 +43,8 @@ const (
 	Health = "health"
 	// Mixer is the name of the mixer plugin passed through the command line
 	Mixer = "mixer"
+	// Cilium is the name of the Cilium plugin passed through the command line
+	Cilium = "cilium"
 )
 
 // ModelProtocolToListenerProtocol converts from a model.Protocol to its corresponding plugin.ListenerProtocol
