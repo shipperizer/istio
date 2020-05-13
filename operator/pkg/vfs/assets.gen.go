75f5a182eeababde16b7b4e4d513371a98d9739d
Jarno Rajahalme <jarno@covalent.io>
2020-05-13 14:58:30 -0700

    manifests: Use default values for Cilium
    Set configuration option defaults for Cilium so that istioctl can be
    used with Cilium without any user-visible changes.
    
    Generated testdata in the next commit!
    
    See 'manifests/UPDATING-CHARTS.md'
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- operator/pkg/vfs/assets.gen.go
+++ operator/pkg/vfs/assets.gen.go
@@ -13667,7 +13667,8 @@ global:
   centralIstiod: false
 
   proxy:
-    image: proxyv2
+    image: docker.io/cilium/istio_proxy:1.6.8
+    enableCilium: true
 
     # cluster domain. Default value is "cluster.local".
     clusterDomain: "cluster.local"
@@ -13812,7 +13813,8 @@ global:
 
   proxy_init:
     # Base name for the proxy_init container, used to configure iptables.
-    image: proxyv2
+    image: docker.io/cilium/istio_proxy:1.6.8
+    dnsProbe: "kube-dns.kube-system.svc.cluster.local"
     resources:
       limits:
         cpu: 100m
@@ -19131,7 +19133,7 @@ pilot:
   tag: ""
 
   # Can be a full hub/image:tag
-  image: pilot
+  image: docker.io/cilium/istio_pilot:1.6.8
   traceSampling: 1.0
 
   # Resources for a small pilot install
@@ -41964,7 +41966,8 @@ spec:
       pilotCertProvider: istiod
       jwtPolicy: third-party-jwt
       proxy:
-        image: proxyv2
+        image: docker.io/cilium/istio_proxy:1.6.8
+        enableCilium: true
         clusterDomain: "cluster.local"
         resources:
           requests:
@@ -41992,7 +41995,8 @@ spec:
           port: # example: 9125
         tracer: "zipkin"
       proxy_init:
-        image: proxyv2
+        image: docker.io/cilium/istio_proxy:1.6.8
+        dnsProbe: "kube-dns.kube-system.svc.cluster.local"
         resources:
           limits:
             cpu: 100m
@@ -42057,7 +42061,7 @@ spec:
       autoscaleMin: 1
       autoscaleMax: 5
       replicaCount: 1
-      image: pilot
+      image: docker.io/cilium/istio_pilot:1.6.8
       traceSampling: 1.0
       configNamespace: istio-config
       appNamespaces: []
